fb0ad933d1542684e97fa1ad76357bc514d9c8d1
Hari.Doshi <hdoshi2@gmail.com>
2019-10-05 01:07:58 -0400

    style: updated footer styling

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { Paper, Tabs, Tab } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+  root: {
+    padding: 20,
+    margin: 20
+  }
+});
 
 const Footer = () => {
+  let classes = useStyles();
   return (
-    <Paper>
-      <Tabs value={1} aria-label="simple tabs example">
-          <Tab label="Item One"/>
-          <Tab label="Item Two" />
-          <Tab label="Item Three" />
-        </Tabs>
+    <Paper className={classes.root}>
+      <Tabs
+        value={0}
+        // onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+        padding="40px"
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+      </Tabs>
     </Paper>
   );
 };
-
 export default Footer;
